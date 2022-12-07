f0395afe96aaa5f92df8c6ab8949c69137e814f8
Gregory Hess <gregory.w.hess@gmail.com>
2022-12-06 21:32:10 -0800

    Removed testing I shouldn't claim

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,4 @@
-use dicecloud_models::data_models::dnd_model::*;
-use std::{fs::File, io::Read};
-fn main(){
-    let mut file = File::open("raulnor_bogdan.json").expect("failed to open file");
-    let mut buf = vec![];
-    let _=file.read_to_end(&mut buf);
-    let char_str = String::from_utf8_lossy(&buf);
-    let character = Character::from_str(&char_str).expect("I really hope it works");
 
+fn main(){
+    println!("future tests");
 }
